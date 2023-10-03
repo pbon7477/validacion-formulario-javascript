c44c4e01f6636e6203b9b315f123f702926449dc
pbon7477 <144088967+pbon7477@users.noreply.github.com>
2023-10-03 03:28:28 -0400

    script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ formulario.addEventListener('submit',(event)=>{
     emailError.classList.remove('error-message');
    }
 
-// validar conteaseña
+// validar contraseña
 
 let passwordEntrada = document.getElementById("password");
 let passwordError = document.getElementById("passwordError");
@@ -125,75 +125,3 @@ db.collection("users").add({
 
 
 
-
-
-
-
-//------- validacion ------
-
-
-/*
-
-
-let formulario = document.getElementById("formulario");
-
-formulario.addEventListener('submit',(event)=>{
-    event.preventDefault();
-   
-// validando nombre
-    let entradaNombre = document.getElementById('name');
-    let errorNombre = document.getElementById('nameError');
-
-    if(entradaNombre.value.trim() === ''){
-        errorNombre.textContent = 'Por favor, introdusca su nombre';
-        errorNombre.classList.add('error-message');
-    }
-    else{
-        errorNombre.textContent = '';
-        errorNombre.classList.remove('error-message');
-    }
-   
-// validar mail
-
-   let emailEntrada = document.getElementById('email');
-   let emailError = document.getElementById('emailError');
-   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-   if(!emailPattern.test(emailEntrada.value)){
-    emailError.textContent = 'Intrudusca un email valido';
-    emailError.classList.add('error-message');
-   }
-   else{
-    emailError.textContent = '';
-    emailError.classList.remove('error-message');
-   }
-
-// validar conteaseña
-
-let passwordEntrada = document.getElementById("password");
-let passwordError = document.getElementById("passwordError");
-let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/;
-
-if(!passwordPattern.test(passwordEntrada.value)){
-   passwordError.textContent = 'La contraseña debe tener al menos 8 caracteres, mayusculas, minusculas, numeros y caracteres especiales.';
-   passwordError.classList.add('error-message');
-
-}
-
-else{
-    passwordError.textContent = '';
-    passwordError.classList.remove('error-message')
-}
-
-
-if(!errorNombre.textContent && !emailError.textContent && !passwordError.textContent){
-     
-    alert('Los datos del formulario se han enviado con exito!');
-    formulario.reset();
-}
-
-
-})
-
-
-*/
